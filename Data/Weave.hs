1aed6e486971747e23ac056138d8225e78a908cd
Craig Roche <cdxr01@gmail.com>
2013-05-30 16:04:31 -0400

    simplified Data.Weave.normalize

--- Data/Weave.hs
+++ Data/Weave.hs
@@ -29,8 +29,9 @@ where
 import Control.Arrow
 import Control.Applicative
 import Data.Monoid
-import Data.Foldable ( Foldable, foldMap )
-import Data.Traversable ( traverse, for, fmapDefault, foldMapDefault )
+import Data.Either      ( lefts, rights )
+import Data.Foldable    ( Foldable, foldMap )
+import Data.Traversable ( Traversable, traverse )
 import qualified Data.Traversable as Tr
 
 
@@ -50,13 +51,13 @@ instance (Show c, Show a) => Show (Weave c a) where
         showString "fromEithers " . shows (toEithers w)
 
 instance Functor (Weave t) where
-    fmap = fmapDefault
+    fmap = Tr.fmapDefault
 
 instance Foldable (Weave t) where
-    foldMap = foldMapDefault
+    foldMap = Tr.foldMapDefault
 
-instance Tr.Traversable (Weave t) where
-    traverse f (Weave es) = Weave <$> for es (pure . Left ||| fmap Right . f)
+instance Traversable (Weave t) where
+    traverse f (Weave es) = Weave <$> Tr.for es (pure . Left ||| fmap Right . f)
 
 instance Monoid (Weave t a) where
     mempty = Weave []
@@ -74,30 +75,39 @@ context = Weave . map Left
 weave :: (c -> b -> b) -> (a -> b -> b) -> b -> Weave c a -> b
 weave fc fa x = foldr (either fc fa) x . toEithers
 
-
-
-normalize :: (Monoid t) => Weave t a -> Weave t a
-normalize = fromEithers . combineLefts . toEithers
-  where
-    isLeft = either (const True) (const False)
-
-    combineLefts es =
-      case es of
-          [] -> []
-          (Right x:xs) -> Right x : combineLefts xs
-          _ ->
-            case span isLeft es of
-                ([r], xs) -> r : combineLefts xs
-                (rs,  xs) -> Left (mconcat (map fromLeft rs)) : combineLefts xs
-    fromLeft (Left a) = a
-    fromLeft (Right  _) = undefined
-
-
 transcribe :: (Monoid t) => (a -> t) -> Weave t a -> t
 transcribe f = mconcat . map (either id f) . toEithers
 
 transcribeA :: (Monoid t, Applicative f) => (a -> f t) -> Weave t a -> f t
 transcribeA f = fmap mconcat . traverse (either pure f) . toEithers
+
+
+normalize :: (Monoid t) => Weave t a -> Weave t a
+normalize = fromEithers . foldMapLefts id . toEithers
+
+
+-- Either helpers ----------
+
+isLeft, isRight :: Either a b -> Bool
+isLeft  = either (const True) (const False)
+isRight = not . isLeft
+
+mapAdjacentEithers :: ([a] -> [c]) -> ([b] -> [c]) -> [Either a b] -> [c]
+mapAdjacentEithers _ _ [] = []
+mapAdjacentEithers fl fr es@(e:_) = case e of
+    Left  _ -> go isLeft  $ fl . lefts
+    Right _ -> go isRight $ fr . rights
+  where
+    go p pull =
+      let (xs, es') = span p es
+      in pull xs ++ mapAdjacentEithers fl fr es'
+
+segment :: [Either a b] -> [Either [a] [b]]
+segment = mapAdjacentEithers ((:[]) . Left) ((:[]) . Right)
+
+foldMapLefts :: (Monoid m) => (a -> m) -> [Either a b] -> [Either m b]
+foldMapLefts f = mapAdjacentEithers ((:[]) . Left . foldMap f) (map Right)
+
 
 test :: Weave Char Int
 test = context "ab" <> unit 23 <> context "c" <> unit 12
